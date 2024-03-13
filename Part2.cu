38f052bae02cd746b0242b7eba471aa73a7579f8
Noah Serhan <99762083+noahs438@users.noreply.github.com>
2024-03-13 11:30:38 -0400

    Update Part2.cu

--- Part2.cu
+++ Part2.cu
@@ -6,3 +6,16 @@
 #include <random>
 #include <time.h>
 #include <iostream>
+
+__global__ void matrixMulKernel(float* P, const float* M, const float* N, int dim) {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (row < dim && col < dim) {
+        float sum = 0.0;
+        for (int k = 0; k < dim; ++k) {
+            sum += M[row * dim + k] * N[k * dim + col];
+        }
+        P[row * dim + col] = sum;
+    }
+}
